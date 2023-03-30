5968caab141f9dd44c4069c1aa3029839e695937
Clément BELLIER <clement.bellier@gmail.com>
2023-03-30 15:19:00 +0200

    everything is set up, not working

--- components/playerPage.jsx
+++ components/playerPage.jsx
@@ -1,6 +1,7 @@
 import Disk from "./disk";
 import "./playerPage.css";
 import TrackCard from "./trackCard";
+import PlayButton from "./playButton";
 
 const PlayerPage = () => {
   return (
@@ -10,7 +11,7 @@ const PlayerPage = () => {
           <div className="song__infos-disk">
             <Disk
               width="180px"
-              position={{ left: "20%", zIndex: "-1"}}
+              position={{ left: "20%", zIndex: "-1" }}
               rotate={true}
             />
             <img src="/lala_dp_front-1024x925.jpg" />
@@ -22,12 +23,33 @@ const PlayerPage = () => {
             <a href="">Spotify</a>
           </div>
         </div>
-        <div className="song__trackslist">
-          <TrackCard />
-          <TrackCard />
-          <TrackCard />
-          <TrackCard />
+        <div className="song__trackslist--container">
+          <div className="song__trackslist--shadow"></div>
+          <div className="song__trackslist">
+            <TrackCard />
+            <TrackCard />
+            <TrackCard />
+            <TrackCard />
+          </div>
         </div>
+      </div>
+      <div className="player-controls">
+        <PlayButton />
+        <div className="loop">
+          <img src="/loop-icon.svg" />
+        </div>
+        <div className="current-time">01:30</div>
+        <input type="range" min="0" max="100" value="90" />
+        <div className="song-time">03:32</div>
+      </div>
+      <div className="other-songs">
+        <p>D'autres titres</p>
+        <img src="lala_dp_front-1024x925.jpg" />
+        <img src="lala_dp_front-1024x925.jpg" />
+        <img src="lala_dp_front-1024x925.jpg" />
+        <img src="lala_dp_front-1024x925.jpg" />
+        <img src="lala_dp_front-1024x925.jpg" />
+        <img src="lala_dp_front-1024x925.jpg" />
       </div>
       <div className="background-player" aria-hidden="true">
         <img src="/arriereplan-lecteur.svg" alt="" />
